96eaa33cb9dec2902cc40366ecf407b67300ae45
ThePowerOfCreation21 <dadekavweb2@gmail.com>
2022-11-19 09:36:39 +0330

    added StudentFinancialResource

---
+++ app/Http/Resources/StudentFinancialResource.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Genocide\Radiocrud\Helpers;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class StudentFinancialResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'amount' => $this->amount,
+            'paid' => $this->paid,
+            'date' => Helpers::getCustomDateCast($this->date),
+        ];
+    }
+}
