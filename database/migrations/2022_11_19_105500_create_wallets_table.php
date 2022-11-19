c90b321f3a3bcc16fb086fd9107d66fc31bb645b
mrcr_geek <mrcrsogand@gmail.com>
2022-11-19 11:29:11 +0330

    add wallet migration

---
+++ database/migrations/2022_11_19_105500_create_wallets_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('wallets', function (Blueprint $table) {
+            $table->id()->startingValue(1000);
+            $table->string('charged_by_id');
+            $table->string('student_id');
+            $table->integer('amount');
+            $table->string('action');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('wallets');
+    }
+};
