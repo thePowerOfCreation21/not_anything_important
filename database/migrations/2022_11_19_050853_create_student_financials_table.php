de6e448ee92a6d7853af87c78376a5cdea58b6d4
ThePowerOfCreation21 <dadekavweb2@gmail.com>
2022-11-19 08:51:41 +0330

    added create_student_financials_table migration

---
+++ database/migrations/2022_11_19_050853_create_student_financials_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('student_financials', function (Blueprint $table) {
+            $table->id()->startingValue(1000);
+            $table->string('student_id', 20);
+            $table->integer('amount');
+            $table->boolean('paid')->default(false);
+            $table->timestamp('date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('student_financials');
+    }
+};
