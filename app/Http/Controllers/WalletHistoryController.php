308d813a91f00e3584ee05486bed59a74fc0c624
mrcr_geek <mrcrsogand@gmail.com>
2022-11-19 17:15:29 +0330

    add storeAmountById Methods

--- app/Http/Controllers/WalletHistoryController.php
+++ app/Http/Controllers/WalletHistoryController.php
@@ -3,8 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\StudentModel;
+use App\Actions\WalletHistoryAction;
 
 class WalletHistoryController extends Controller
 {
-    //
+    public function store(Request $request, $id)
+    {
+        return response()->json([
+            'message' => 'ok',
+            'data' => (new WalletHistoryAction())
+                ->setRequest($request)
+                ->setValidationRule('store')
+                ->storeAmountById($id)
+        ]);
+    }
 }
