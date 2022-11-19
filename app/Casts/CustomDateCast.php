cae6ff4a2cba42c29348d241785210e004c4d2ea
ThePowerOfCreation21 <dadekavweb2@gmail.com>
2022-11-19 08:57:50 +0330

    added CustomDateCast

---
+++ app/Casts/CustomDateCast.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Casts;
+
+use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
+use Genocide\Radiocrud\Helpers;
+
+class CustomDateCast implements CastsAttributes
+{
+    /**
+     * Cast the given value.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @param  string  $key
+     * @param  mixed  $value
+     * @param  array  $attributes
+     * @return mixed
+     */
+    public function get($model, string $key, $value, array $attributes)
+    {
+        return empty($value) ? null : Helpers::timeToCustomDate(
+            strtotime($value)
+        );
+    }
+
+    /**
+     * Prepare the given value for storage.
+     *
+     * @param  \Illuminate\Database\Eloquent\Model  $model
+     * @param  string  $key
+     * @param  mixed  $value
+     * @param  array  $attributes
+     * @return mixed
+     */
+    public function set($model, string $key, $value, array $attributes)
+    {
+        return  Helpers::setCustomDateCast($value);
+    }
+}
