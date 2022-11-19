73b93025c2c96e5637fcd9ea884a22d84368fe8a
mrcr_geek <mrcrsogand@gmail.com>
2022-11-19 17:15:00 +0330

    add storeAmountById route

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
+use App\Http\Controllers\WalletHistoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,3 +21,4 @@ Route::post('/student/register_request', [StudentController::class, 'registerReq
 Route::post('/admin/student', [StudentController::class, 'storeByAdmin']); //TODO: require admin authorization for this route
 Route::post('/admin/student/{id}/block', [StudentController::class, 'block']); //TODO: require admin authorization for this route
 Route::get('/admin/student/{id}/unblock', [StudentController::class, 'unblock']); //TODO: require admin authorization for this route
+Route::post('/admin/student/wallet/{id}', [WalletHistoryController::class, 'store']); //TODO: require admin authorization for this route
