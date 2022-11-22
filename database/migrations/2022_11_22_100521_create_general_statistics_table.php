65c290aa59a754dd32967e2abe303a732ac18cb3
ThePowerOfCreation21 <dadekavweb2@gmail.com>
2022-11-22 10:14:00 +0330

    added create_general_statistics_table migration

---
+++ database/migrations/2022_11_22_100521_create_general_statistics_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('general_statistics', function (Blueprint $table) {
+            $table->id();
+            $table->string('label', 250);
+            $table->bigInteger('paid')->default(0);
+            $table->bigInteger('not_paid')->default(0);
+            $table->string('educational_year')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('general_statistics');
+    }
+};
