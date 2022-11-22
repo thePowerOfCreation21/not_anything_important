92120299b8a421bc26b77027e1690d8ff02bbc78
ThePowerOfCreation21 <dadekavweb2@gmail.com>
2022-11-22 13:03:09 +0330

    added admin auth to routes

--- routes/api.php
+++ routes/api.php
@@ -18,27 +18,32 @@ use App\Http\Controllers\AdminController;
 |
 */
 
-Route::post('/admin/register', [AdminController::class, 'register']); //TODO: require admin authorization for this route
+
 Route::post('/admin/login', [AdminController::class, 'login']); //TODO: require admin authorization for this route
 
 Route::post('/student/register_request', [StudentController::class, 'registerRequest']);
 
-Route::post('/admin/student', [StudentController::class, 'storeByAdmin']); //TODO: require admin authorization for this route
-Route::put('/admin/student/{id}', [StudentController::class, 'updateById']); //TODO: require admin authorization for this route
-Route::post('/admin/student/{id}/block', [StudentController::class, 'block']); //TODO: require admin authorization for this route
-Route::get('/admin/student/{id}/unblock', [StudentController::class, 'unblock']); //TODO: require admin authorization for this route
+Route::group([
+    'middleware' => ['auth:admin']
+], function(){
+    Route::post('/admin/register', [AdminController::class, 'register']);
+    Route::post('/admin/student', [StudentController::class, 'storeByAdmin']);
+    Route::put('/admin/student/{id}', [StudentController::class, 'updateById']);
+    Route::post('/admin/student/{id}/block', [StudentController::class, 'block']);
+    Route::get('/admin/student/{id}/unblock', [StudentController::class, 'unblock']);
 
-Route::post('/admin/student/wallet', [WalletHistoryController::class, 'store']); //TODO: require admin authorization for this route
-Route::get('/admin/student/wallet', [WalletHistoryController::class, 'get']); //TODO: require admin authorization for this route
+    Route::post('/admin/student/wallet', [WalletHistoryController::class, 'store']);
+    Route::get('/admin/student/wallet', [WalletHistoryController::class, 'get']);
 
-Route::post('/admin/teacher/skill', [TeacherSkillController::class, 'storeByAdmin']);//TODO: same as others
-Route::post('/admin/teacher/skill/{id}', [TeacherSkillController::class, 'updateById']);//TODO: same as others
-Route::delete('/admin/teacher/skill/{id}', [TeacherSkillController::class, 'deleteById']);//TODO: same as others
-Route::get('/admin/teacher/skill', [TeacherSkillController::class, 'get']);//TODO: same as others
-Route::get('/admin/teacher/skill/{id}', [TeacherSkillController::class, 'getById']);//TODO: same as others
+    Route::post('/admin/teacher/skill', [TeacherSkillController::class, 'storeByAdmin']);
+    Route::post('/admin/teacher/skill/{id}', [TeacherSkillController::class, 'updateById']);
+    Route::delete('/admin/teacher/skill/{id}', [TeacherSkillController::class, 'deleteById']);
+    Route::get('/admin/teacher/skill', [TeacherSkillController::class, 'get']);
+    Route::get('/admin/teacher/skill/{id}', [TeacherSkillController::class, 'getById']);
 
-Route::post('/admin/student/financial', [StudentFinancialController::class, 'store']); //TODO: require admin authorization for this route
-Route::put('/admin/student/financial/{id}', [StudentFinancialController::class, 'updateById']); //TODO: require admin authorization for this route
-Route::delete('/admin/student/financial/{id}', [StudentFinancialController::class, 'deleteById']); //TODO: require admin authorization for this route
-Route::get('/admin/student/financial', [StudentFinancialController::class, 'get']); //TODO: require admin authorization for this route
-Route::get('/admin/student/financial/{id}', [StudentFinancialController::class, 'getById']); //TODO: require admin authorization for this route
+    Route::post('/admin/student/financial', [StudentFinancialController::class, 'store']);
+    Route::put('/admin/student/financial/{id}', [StudentFinancialController::class, 'updateById']);
+    Route::delete('/admin/student/financial/{id}', [StudentFinancialController::class, 'deleteById']);
+    Route::get('/admin/student/financial', [StudentFinancialController::class, 'get']);
+    Route::get('/admin/student/financial/{id}', [StudentFinancialController::class, 'getById']);
+});
