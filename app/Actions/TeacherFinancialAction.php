0d0d08d8d17223b8dbafd36a1be9ffd6a0fa0e85
mrcr_geek <mrcrsogand@gmail.com>
2022-11-26 12:01:04 +0330

    add teacherFinancial action

---
+++ app/Actions/TeacherFinancialAction.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Actions;
+
+use Genocide\Radiocrud\Services\ActionService\ActionService;
+use App\Models\TeacherFinancialModel;
+use App\Helpers\PardisanHelper;
+use App\Http\Resources\TeacherFinancialResource;
+
+class TeacherFinancialAction extends ActionService
+{
+
+}
