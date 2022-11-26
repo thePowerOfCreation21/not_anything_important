c02262df87da3f8cc76c45017b218608ebc0d45e
mrcr_geek <mrcrsogand@gmail.com>
2022-11-26 12:00:25 +0330

    add TeacherFinancial controller

---
+++ app/Http/Controllers/TeacherFinancialController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Helpers\PardisanHelper;
+use Genocide\Radiocrud\Exceptions\CustomException;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Request;
+use App\Models\TeacherFinancialModel;
+
+class TeacherFinancialController extends Controller
+{
+
+}
