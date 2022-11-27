b2461bd8a61f188570355dcd143fe6a422c1226c
mrcr_geek <mrcrsogand@gmail.com>
2022-11-28 01:05:11 +0330

    update StudentDisciplineModel

--- app/Models/StudentDisciplineModel.php
+++ app/Models/StudentDisciplineModel.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class StudentDisciplineModel extends Model
 {
@@ -20,4 +21,9 @@ class StudentDisciplineModel extends Model
     ];
 
     public $timestamps = false;
+
+    public function student (): BelongsTo
+    {
+        return $this->belongsTo(StudentModel::class, 'student_id', 'id');
+    }
 }
