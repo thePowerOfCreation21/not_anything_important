20ffd1fb648de05bcf93724300923f869fc6106d
mrcr_geek <mrcrsogand@gmail.com>
2022-11-27 18:38:48 +0330

    add StudentDiscipline migration

---
+++ database/migrations/2022_11_27_181831_create_student_disciplines_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('student_disciplines', function (Blueprint $table) {
+            $table->id()->startingValue(1000);
+            $table->string('student_id', '20');
+            $table->string('title', '250');
+            $table->string('description', '500');
+            $table->timestamps('date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('student_disciplines');
+    }
+};
