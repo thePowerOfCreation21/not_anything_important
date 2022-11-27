41ce8ab13645d248fc80a25c34b032063329a37a
mrcr_geek <mrcrsogand@gmail.com>
2022-11-27 19:47:56 +0330

    add storeStudentDiscipline methods(action&controller)

--- app/Actions/StudentDisciplineAction.php
+++ app/Actions/StudentDisciplineAction.php
@@ -10,8 +10,26 @@ class StudentDisciplineAction extends ActionService
     public function __construct()
     {
         $this
-            ->setModel(StudentDisciplineModel::class);
+            ->setModel(StudentDisciplineModel::class)
+            ->setValidationRules([
+                'store' => [
+                    'student_id' => ['required', 'string', 'max:20'],
+                    'title' => ['required', 'string', 'max:250'],
+                    'description' => ['required', 'string', 'max:500'],
+                    'date' => ['required', 'date_format:Y-m-d']
+                ]
+            ])
+            ->setCasts([
+                'date' => ['jalali_to_gregorian:Y-m-d']
+            ]);
 
         parent::__construct();
+    }
+
+    public function store(array $data, callable $storing = null): mixed
+    {
+        $data['educational_year'] = PardisanHelper::getEducationalYearByGregorianDate($data['date']);
+
+        return parent::store($data, $storing);
     }
 }
--- app/Http/Controllers/StudentDisciplineController.php
+++ app/Http/Controllers/StudentDisciplineController.php
@@ -2,11 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Actions\StudentDisciplineAction;
+use Genocide\Radiocrud\Exceptions\CustomException;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 use App\Models\StudentDisciplineModel;
 use App\Actions\StudentFinancialAction;
 
 class StudentDisciplineController extends Controller
 {
-    //
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     * @throws CustomException
+     */
+    public function store(Request $request): JsonResponse
+    {
+        return response()->json([
+           'message' => 'ok',
+           'data' => (new StudentDisciplineAction())
+                ->setRequest($request)
+                ->setValidationRule('store')
+                ->storeByRequest()
+        ]);
+    }
 }
