30c7f207609f9249d914cbc8d9210868f7f80e40
mrcr_geek <mrcrsogand@gmail.com>
2022-11-27 18:40:07 +0330

    add StudentDiscipline controller

---
+++ app/Http/Controllers/StudentDisciplineController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\StudentDisciplineModel;
+use App\Actions\StudentFinancialAction;
+
+class StudentDisciplineController extends Controller
+{
+    //
+}
