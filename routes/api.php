2896eb39cb95656f8e1ef4e39ae0f01cc40e86ef
mrcr_geek <mrcrsogand@gmail.com>
2022-11-28 12:59:34 +0330

    add updateStudentDiscipline route

--- routes/api.php
+++ routes/api.php
@@ -81,4 +81,5 @@ Route::group([
     Route::post('/admin/student/discipline', [StudentDisciplineController::class, 'store']);
     Route::get('/admin/students/discipline', [StudentDisciplineController::class, 'get']);
     Route::get('/admin/students/discipline/{id}', [StudentDisciplineController::class, 'getById']);
+    Route::put('/admin/students/discipline/{id}', [StudentDisciplineController::class, 'updateById']);
 });
