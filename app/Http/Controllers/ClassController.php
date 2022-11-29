50e04a791fa2a086ab747b13a893fb5d1b2cc002
mani-ebrahimi31 <maniebrahimi31@gmail.com>
2022-11-29 14:53:32 +0330

    Added StoreByAdmin Function Request in ClassController

--- app/Http/Controllers/ClassController.php
+++ app/Http/Controllers/ClassController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Actions\ClassAction;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class ClassController extends Controller
 {
-    //
+    public function storeByAdmin(Request $request): JsonResponse
+    {
+        return response()->json([
+            'message' => 'Ok',
+            'data' => (new ClassAction())
+                ->setRequest($request)
+                ->setValidationRule('storeByAdmin')
+                ->storeByRequest()
+        ]);
+    }
 }
