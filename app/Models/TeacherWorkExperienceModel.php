8cc876637786c0a5a82c0e3b230a674a48ccbbf7
unknown <iamiraskari@gmail.com>
2022-11-30 10:19:28 +0330

    added teacher work exp migration and model

---
+++ app/Models/TeacherWorkExperienceModel.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TeacherWorkExperienceModel extends Model
+{
+
+    use HasFactory;
+    protected $table = 'teacher_work_experience';
+
+    protected $fillable = [
+        'teacher_id',
+        'title',
+        'workplace_name',
+        'work_title',
+        'current_status',
+        'reason_for_the_termination_of_cooperation',
+        'workplace_location',
+    ];
+
+    public $timestamps = false;
+}
