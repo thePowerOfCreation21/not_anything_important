5e6894163ded309344c7e5dab633fe150fec9810
ThePowerOfCreation21 <dadekavweb2@gmail.com>
2022-12-01 18:20:03 +0330

    added new fields to StudentFinancialResource

--- app/Http/Resources/StudentFinancialResource.php
+++ app/Http/Resources/StudentFinancialResource.php
@@ -17,10 +17,15 @@ class StudentFinancialResource extends JsonResource
     {
         return [
             'id' => $this->id,
+            'payment_type' => $this->payment_type,
+            'check_number' => $this->check_number,
+            'receipt_number' => $this->receipt_number,
             'amount' => $this->amount,
             'paid' => $this->paid,
             'date' => Helpers::getCustomDateCast($this->date),
+            'payment_date' => Helpers::getCustomDateCast($this->payment_date),
             'educational_year' => $this->educational_year,
+            'check_image' => $this->check_image,
             'student_id' => $this->student_id,
             'student' => new StudentResource($this->whenLoaded('student'))
         ];
