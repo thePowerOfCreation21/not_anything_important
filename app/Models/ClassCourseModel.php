3991bfce83bf8b008f53af1187b903780a791ceb
ThePowerOfCreation21 <dadekavweb2@gmail.com>
2022-12-02 00:50:37 +0330

    added ClassCourseModel

---
+++ app/Models/ClassCourseModel.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class ClassCourseModel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'class_course';
+
+    protected $fillable = [
+        'class_id',
+        'course_id',
+        'teacher_id'
+    ];
+
+    public $timestamps = false;
+
+    public function classModel (): BelongsTo
+    {
+        return $this->belongsTo(ClassModel::class, 'class_id', 'id');
+    }
+
+    public function course (): BelongsTo
+    {
+        return $this->belongsTo(CourseModel::class, 'class_id', 'id');
+    }
+
+    public function teacher (): BelongsTo
+    {
+        return $this->belongsTo(TeacherModel::class, 'class_id', 'id');
+    }
+}
