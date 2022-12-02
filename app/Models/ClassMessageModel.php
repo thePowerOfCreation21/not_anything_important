54e4ee20860ca08df63bd92f481cfbbd8e659c62
mani-ebrahimi31 <maniebrahimi31@gmail.com>
2022-12-02 14:54:02 +0330

    Added (tableName, fillable) in ClassMessageModel

--- app/Models/ClassMessageModel.php
+++ app/Models/ClassMessageModel.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class ClassMessageModel extends Model
 {
     use HasFactory;
+
+    protected $table = 'class_messages';
+
+    protected $fillable = [
+        'student_id',
+        'class_id',
+        'text'
+    ];
 }
