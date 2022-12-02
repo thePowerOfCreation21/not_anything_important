06794b32e30b37d40f62acce036a3356a92bbc4a
mani-ebrahimi31 <maniebrahimi31@gmail.com>
2022-12-02 12:54:01 +0330

    Added (setModel, setResource, setValidationRules) in ClassReportsAction

---
+++ app/Actions/ClassReportsAction.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Actions;
+
+
+use App\Models\ClassReportsModel;
+use App\Http\Resources\ClassResource;
+use Genocide\Radiocrud\Services\ActionService\ActionService;
+
+class ClassAction extends ActionService
+{
+    public function __construct()
+    {
+        $this
+            ->setModel(ClassReportsModel::class)
+            ->setResource(ClassResource::class)
+            ->setValidationRules([
+                'store' => [
+                    'telegram' => ['required', 'string', 'min:2', 'max:5000']
+                ],
+                'update' => [
+                    'telegram' => ['string', 'min:2', 'max:5000']
+                ]
+            ]);
+        parent::__construct();
+    }
+}
