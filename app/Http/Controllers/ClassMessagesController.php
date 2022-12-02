81631e64a5c885aad7934ea27125cbe87d547db2
mani-ebrahimi31 <maniebrahimi31@gmail.com>
2022-12-02 15:11:01 +0330

    Added store function in ClassMessagesController

--- app/Http/Controllers/ClassMessagesController.php
+++ app/Http/Controllers/ClassMessagesController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Actions\ClassMessagesAction;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class ClassMessagesController extends Controller
 {
-    //
+    public function store(Request $request): JsonResponse
+    {
+        return response()->json([
+            'message' => 'Ok',
+            'data' => (new ClassMessagesAction())
+                ->setRequest($request)
+                ->setValidationRule('store')
+                ->storeByRequest()
+        ]);
+    }
 }
