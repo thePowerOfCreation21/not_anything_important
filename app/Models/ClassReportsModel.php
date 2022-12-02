0847687f60df2c445db93bb9d67e8a127128db4c
mani-ebrahimi31 <maniebrahimi31@gmail.com>
2022-12-02 12:40:37 +0330

    Added (tableName, fillable) in ClassReportsModel

--- app/Models/ClassReportsModel.php
+++ app/Models/ClassReportsModel.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class ClassReportsModel extends Model
 {
     use HasFactory;
+
+    protected $table = 'class_reports';
+
+    protected $fillable = [
+        'telegram'
+    ];
 }
