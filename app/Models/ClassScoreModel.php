311404ce569bf5496ef2c9fddc11324c6cee8276
mani-ebrahimi31 <maniebrahimi31@gmail.com>
2022-12-03 16:59:15 +0330

    Added (tableName, fillable) in ClassMessageModel

--- app/Models/ClassScoreModel.php
+++ app/Models/ClassScoreModel.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class ClassScoreModel extends Model
 {
     use HasFactory;
+
+    protected $table = 'class_score';
+
+    protected $fillable = [
+        'class_course_id',
+        'date',
+        'educational_year',
+        'max_score'
+    ];
 }
