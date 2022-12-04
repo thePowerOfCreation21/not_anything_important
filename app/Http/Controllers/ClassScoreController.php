be7cc500c73c642fe647f4436ea7abb63820fedb
mani-ebrahimi31 <maniebrahimi31@gmail.com>
2022-12-04 17:49:47 +0330

    Added StoreByAdmin functionality in ClassScoreController

--- app/Http/Controllers/ClassScoreController.php
+++ app/Http/Controllers/ClassScoreController.php
@@ -2,9 +2,20 @@
 
 namespace App\Http\Controllers;
 
+use App\Actions\ClassScoreAction;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class ClassScoreController extends Controller
 {
-    //
+    public function storeByAdmin (Request $request): JsonResponse
+    {
+        return response()->json([
+            'message' => 'ok',
+            'data' => (new ClassScoreAction())
+                ->setRequest($request)
+                ->setValidationRule('storeByAdmin')
+                ->storeByRequest()
+        ]);
+    }
 }
