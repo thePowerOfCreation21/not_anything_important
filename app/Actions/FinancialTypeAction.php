1d508f8e34fa8020a194f73826da065648eb4480
mrcr_geek <mrcrsogand@gmail.com>
2022-12-10 13:55:04 +0330

    add FinancialAction

---
+++ app/Actions/FinancialTypeAction.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Actions;
+
+use App\Http\Resources\FinancialTypeResource;
+use Genocide\Radiocrud\Services\ActionService\ActionService;
+use App\Models\FinancialType;
+
+class FinancialTypeAction extends ActionService
+{
+    public function __construct()
+    {
+        $this
+            ->setModel(FinancialType::class)
+            ->setResource(FinancialTypeResource::class)
+            ->setValidationRules([
+                'store' => [
+                    'title' => ['required', 'string', 'max:255']
+                ]
+            ]);
+
+        parent::__construct();
+    }
+}
