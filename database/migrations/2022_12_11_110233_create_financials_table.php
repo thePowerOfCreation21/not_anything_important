80d7d4df1995d1a662f49a59b2016477b4acdb1c
mrcr_geek <mrcrsogand@gmail.com>
2022-12-11 11:17:23 +0330

    add FinancialMigration

---
+++ database/migrations/2022_12_11_110233_create_financials_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('financials', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 255);
+            $table->string('description', 500);
+            $table->string('financial_type_id', 20);
+            $table->integer('amount', 20);
+            $table->timestamp('date');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('financials');
+    }
+};
