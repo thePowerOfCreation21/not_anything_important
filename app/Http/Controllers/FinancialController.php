f291b6d8de18a00ae483df741f984dcc0f49aed5
mrcr_geek <mrcrsogand@gmail.com>
2022-12-11 11:17:48 +0330

    add FinancialController

---
+++ app/Http/Controllers/FinancialController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Financial;
+use App\Actions\FinancialAction;
+use Illuminate\Http\JsonResponse;
+use Genocide\Radiocrud\Exceptions\CustomException;
+
+class FinancialController extends Controller
+{
+    //
+}
