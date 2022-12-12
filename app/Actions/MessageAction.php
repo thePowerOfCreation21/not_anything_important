602b4310f8ed8afe33ec381c9540e1789c03bdc0
mrcr_geek <mrcrsogand@gmail.com>
2022-12-12 20:36:01 +0330

    add Message controller&action

---
+++ app/Actions/MessageAction.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Actions;
+
+use Genocide\Radiocrud\Services\ActionService\ActionService;
+
+class MessageAction extends ActionService
+{
+    public function __construct()
+    {
+        
+        parent::__construct();
+    }
+}
