a52e26b97c8c50d2fc8865135a1c4bf79394bf1f
mrcr_geek <mrcrsogand@gmail.com>
2022-12-12 22:30:20 +0330

    add message(store) methods(action&controller)

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -3,8 +3,26 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Message;
+use App\Actions\MessageAction;
+use Illuminate\Http\JsonResponse;
+use Genocide\Radiocrud\Exceptions\CustomException;
 
 class MessageController extends Controller
 {
-    //
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     * @throws CustomException
+     */
+    public function store(Request $request): JsonResponse
+    {
+        return response()->json([
+            'message' => 'ok',
+            'data' => (new MessageAction())
+                ->setRequest($request)
+                ->setValidationRule('store')
+                ->storeByRequest()
+        ]);
+    }
 }
