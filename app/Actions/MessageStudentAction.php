6ac46dee01bdbeff474239ad26d2a6e3d0085ea5
mrcr_geek <mrcrsogand@gmail.com>
2022-12-12 20:39:43 +0330

    add MessageStudent Action

---
+++ app/Actions/MessageStudentAction.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Actions;
+
+use Genocide\Radiocrud\Services\ActionService\ActionService;
+
+class MessageStudentAction extends ActionService
+{
+    public function __construct()
+    {
+        
+        parent::__construct();
+    }
+}
