d7d19019b3b5270cf9db93d493a726daeb3afff5
mrcr_geek <mrcrsogand@gmail.com>
2022-12-18 13:52:31 +0330

    add AdviceDate Action

---
+++ app/Actions/AdviceDateAction.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Actions;
+
+use Genocide\Radiocrud\Services\ActionService\ActionService;
+
+class AdviceDateAction extends ActionService
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+}
