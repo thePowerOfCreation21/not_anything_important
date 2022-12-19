bc3cf23ff83b7e7536e501c54777fec7819b3104
ThePowerOfCreation21 <dadekavweb2@gmail.com>
2022-12-19 15:11:48 +0330

    added create inventory_products table migration

---
+++ database/migrations/2022_12_19_145603_create_inventory_products_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('inventory_products', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 250);
+            $table->string('code', 50)->unique();
+            $table->integer('stock')->unsigned()->default(0);
+            $table->string('description', 2500)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('inventory_products');
+    }
+};
