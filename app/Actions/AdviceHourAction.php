cb732ac9f2477bc694c9aac6c2c73e42264e6e46
mrcr_geek <mrcrsogand@gmail.com>
2022-12-19 17:21:49 +0330

    add AdviceHour action&controller

---
+++ app/Actions/AdviceHourAction.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Actions;
+
+use Genocide\Radiocrud\Services\ActionService\ActionService;
+use App\Models\AdviceHourModel;
+
+class AdviceHourAction extends ActionService
+{
+    public function __construct()
+    {
+
+        parent::__construct();
+    }
+}
---
+++ app/Http/Controllers/AdviceHourController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\AdviceHourModel;
+use App\Actions\AdviceHourAction;
+use Illuminate\Http\JsonResponse;
+use Genocide\Radiocrud\Exceptions\CustomException;
+
+class AdviceHourController extends Controller
+{
+    //
+}
