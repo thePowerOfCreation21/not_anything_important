8f1f488baf6af1091f35b65aa94cc2d764f98ddc
mrcr_geek <mrcrsogand@gmail.com>
2022-12-19 22:45:48 +0330

    add AdviceHour(store) method(controller)

--- app/Actions/AdviceHourAction.php
+++ app/Actions/AdviceHourAction.php
@@ -2,6 +2,7 @@
 
 namespace App\Actions;
 
+use App\Http\Resources\AdviceHourResource;
 use Genocide\Radiocrud\Services\ActionService\ActionService;
 use App\Models\AdviceHourModel;
 
@@ -9,7 +10,14 @@ class AdviceHourAction extends ActionService
 {
     public function __construct()
     {
-
+        $this
+            ->setModel(AdviceHourModel::class)
+            ->setResource(AdviceHourResource::class)
+            ->setValidationRules([
+                'store' => [
+                    'hour' => ['required', 'date_format:H:i:s']
+                ]
+            ]);
         parent::__construct();
     }
 }
--- app/Http/Controllers/AdviceHourController.php
+++ app/Http/Controllers/AdviceHourController.php
@@ -10,5 +10,20 @@ use Genocide\Radiocrud\Exceptions\CustomException;
 
 class AdviceHourController extends Controller
 {
-    //
+
+    /**
+     * @param Request $request
+     * @return JsonResponse
+     * @throws CustomException
+     */
+    public function store(Request $request): JsonResponse
+    {
+        return response()->json([
+           'message' => 'ok',
+           'data' => (new AdviceHourAction())
+                ->setRequest($request)
+                ->setValidationRule('store')
+                ->storeByRequest()
+        ]);
+    }
 }
