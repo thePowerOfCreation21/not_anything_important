49dc8dc3c7d0aab1864118e43b3a1c753c2e372c
ThePowerOfCreation21 <dadekavweb2@gmail.com>
2022-12-22 07:45:39 +0330

    added create teacher_entrances_table migration

---
+++ database/migrations/2022_12_22_072400_create_teacher_entrances_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('teacher_entrances', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('teacher_id')->unsigned();
+            $table->foreign('teacher_id')->references('id')->on('teachers')->onDelete('cascade');
+            $table->tinyInteger('week_day');
+            $table->time('entrance');
+            $table->time('exit');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('teacher_entrances');
+    }
+};
