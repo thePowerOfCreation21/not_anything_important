65a1139570675305f5c1f787e4e6f716ef3b00f2
ThePowerOfCreation21 <dadekavweb2@gmail.com>
2022-12-25 10:02:28 +0330

    casing advice date

--- app/Http/Resources/AdviceDateResource.php
+++ app/Http/Resources/AdviceDateResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use Genocide\Radiocrud\Helpers;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class AdviceDateResource extends JsonResource
@@ -14,6 +15,10 @@ class AdviceDateResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->id,
+            'date' => Helpers::getCustomDateCast($this->date),
+            'educational_year' => $this->educational_year
+        ];
     }
 }
