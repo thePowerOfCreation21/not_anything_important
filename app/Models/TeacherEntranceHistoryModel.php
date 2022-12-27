084ce0c67cbaea79dd55757d97c690d177efe404
ThePowerOfCreation21 <dadekavweb2@gmail.com>
2022-12-27 12:32:04 +0330

    added TeacherEntranceHisstoryController

---
+++ app/Models/TeacherEntranceHistoryModel.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class TeacherEntranceHistoryModel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'teacher_entrance_histories';
+
+    protected $fillable = [
+        'teacher_id',
+        'entrance',
+        'exit',
+        'date'
+    ];
+
+    public $timestamps = false;
+
+    public function teacher (): BelongsTo
+    {
+        return $this->belongsTo(TeacherModel::class, 'teacher_id', 'id');
+    }
+}
