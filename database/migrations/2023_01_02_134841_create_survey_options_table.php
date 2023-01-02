e7f3d769f344edbcf273dc01a8fa2ce3d18929b3
ThePowerOfCreation21 <dadekavweb2@gmail.com>
2023-01-02 13:54:24 +0330

    added create survey_options table migration

---
+++ database/migrations/2023_01_02_134841_create_survey_options_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('survey_options', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('survey_id')->unsigned();
+            $table->foreign('survey_id')->references('id')->on('surveys')->onDelete('cascade');
+            $table->string('text', 2500);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('survey_options');
+    }
+};
