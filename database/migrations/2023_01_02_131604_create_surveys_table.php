24229816677f4ca918e036cab90cf64f0231226b
ThePowerOfCreation21 <dadekavweb2@gmail.com>
2023-01-02 13:29:48 +0330

    added create surveys table migration

---
+++ database/migrations/2023_01_02_131604_create_surveys_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('surveys', function (Blueprint $table) {
+            $table->id();
+            $table->bigInteger('teacher_id')->unsigned()->nullable();
+            $table->foreign('teacher_id')->references('id')->on('teachers')->onDelete('cascade');
+            $table->string('text', 5000);
+            $table->boolean('is_active')->default(false);
+            $table->boolean('is_template')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('surveys');
+    }
+};
