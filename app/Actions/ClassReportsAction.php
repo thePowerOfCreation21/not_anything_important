3afc9c36403bc725b0180bb1689800c7ccab3c8d
ThePowerOfCreation21 <dadekavweb2@gmail.com>
2023-01-03 11:13:32 +0330

    added some queries to get class reports

--- app/Actions/ClassReportsAction.php
+++ app/Actions/ClassReportsAction.php
@@ -26,10 +26,38 @@ class ClassReportsAction extends ActionService
                     'period' => ['integer', 'between:1,10'],
                     'class_course_id' => ['string', 'max:20'],
                     'report' => ['string', 'max:5000']
+                ],
+                'getQuery' => [
+                    'from_date' => ['date_format:Y-m-d'],
+                    'to_date' => ['date_format:Y-m-d'],
+                    'class_course_id' => ['integer'],
+                    'class_id' => ['integer']
                 ]
             ])
             ->setCasts([
-                'date' => ['jalali_to_gregorian:Y-m-d']
+                'date' => ['jalali_to_gregorian:Y-m-d'],
+                'from_date' => ['jalali_to_gregorian:Y-m-d'],
+                'to_date' => ['jalali_to_gregorian:Y-m-d'],
+            ])
+            ->setQueryToEloquentClosures([
+                'from_date' => function (&$eloquent, $query)
+                {
+                    $eloquent = $eloquent->whereDate('date', '>=', $query['from_date']);
+                },
+                'to_date' => function (&$eloquent, $query)
+                {
+                    $eloquent = $eloquent->whereDate('date', '<=', $query['from_date']);
+                },
+                'class_course_id' => function (&$eloquent, $query)
+                {
+                    $eloquent = $eloquent->where('class_course_id', $query['class_course_id']);
+                },
+                'class_id' => function (&$eloquent, $query)
+                {
+                    $eloquent = $eloquent->whereHas('classCourse', function ($q) use($query){
+                        $q->where('class_id', $query['class_id']);
+                    });
+                }
             ]);
         parent::__construct();
     }
