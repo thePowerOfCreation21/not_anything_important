fb9d689b12f6d1e8fa54770ea5cdffde8864f1b8
ThePowerOfCreation21 <dadekavweb2@gmail.com>
2023-01-10 10:55:42 +0330

    added create news table migration

---
+++ database/migrations/2023_01_10_104613_create_news_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('news', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 250);
+            $table->text('content')->nullable();
+            $table->string('image', 250);
+            $table->boolean('is_important')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('news');
+    }
+};
